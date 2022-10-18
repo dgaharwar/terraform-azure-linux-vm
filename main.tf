098bc2cab4bf0a71307baf3738250d4e88c1bc4e
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2022-10-18 10:00:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.27.0"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.name_prefix}-rg"
   location = "${var.location}"
