26d55466bea9935e141a521c02530d6f1d02b97e
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2022-10-18 16:02:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  version         = "3.27.0"
+  subscription_id = "${var.subscription_id}"
+  tenant_id       = "${var.tenant_id}"
+  client_id       = "${var.client_id}"
+  secret_value    = "${var.secret_value}"
+}
+  
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.name_prefix}-rg"
   location = "${var.location}"
